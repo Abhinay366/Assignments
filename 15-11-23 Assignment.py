0aeeebf064f5ebeb2f2b3aa516a9e053b3fa858e
Abhinay366 <abhinay.marolix@gmail.com>
2023-11-16 13:03:01 +0530

    Seventh file

---
+++ 15-11-23 Assignment.py
@@ -0,0 +1,110 @@
+#Reverse a string program.
+##str1=input("enter the sentence:")
+#words=str1.split()
+#rev_words=words[::-1]
+#sen=" ".join(rev_words)
+#print(sen)
+
+#Remove duplicates program.g
+#def remove_dup(string):
+   #lst=[]
+   #for char in string:
+     #if char not in lst:
+         #lst.append(char)
+   ##st="".join(lst)
+   #return st
+#s="ABCCCDDDEFGGGHHHIIJJJKLLABCDEF"
+#print(remove_dup(s))
+
+s = "Abhinay"
+x = list(s)
+print(x)
+
+s = "Abhinay is a developer"
+x = list(s)
+print(x)
+
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)
+
+l = ['a','b','c']
+e = [4, 5]
+l += e
+print(l)
+
+l=[10,20,30,[40,50,60],70,80,90]
+print(l[2],l[3][0])
+
+l=[10,20,30,[40,50,60],70,80,90]
+print(l[4:5])
+
+#Python string Functions.
+
+#1.s.capitalize() 
+#Capitalizes a string.
+
+#2.s.lower() 
+#Converts all alphabetic characters to lowercase.
+
+#3.s.upper() 
+#Converts all alphabetic characters to uppercase.
+
+#4. s.title() 
+#Converts the first letter of each word to uppercase and remaining letters to lowercase.
+
+#5.s.swapcase() 
+#Swaps case of all alphabetic characters.
+
+#6.s.count(<sub>[, <start>[, <end>]])
+#Returns the number of time <sub> occurs in s.
+
+#7.s.find(<sub>[, <start>[, <end>]])
+#Returns the index of the first occurrence of <sub> in s. Returns -1 if the substring is not present in the string.
+
+#8.s.isalnum() 
+#Returns True if all characters of the string are alphanumeric, i.e., letters or numbers.
+
+#9.s.isalpha()
+#Returns True if all the characters in the string are alphabets.
+
+#10.s.isdigit()
+#Returns True if all the characters in the string are numbers.
+
+#11.s.islower()
+#Returns True if all the alphabets in the string are lowercase.
+
+#12.s.isupper()
+#Returns True if all the alphabets in the string are uppercase.
+
+#13.s.lstrip([<chars>])
+#Removes leading characters from a string. Removes leading whitespaces if you don’t provide a <chars> argument.
+
+#14.s.rstrip([<chars>])
+#Removes trailing characters from a string. Removes trailing whitespaces if you don’t provide a <chars> argument.
+
+#15. s.strip([<chars>])
+#Removes leading and trailing characters from a string. Removes leading and trailing whitespaces if you don’t provide a <chars> argument.
+
+#16. s.join(<iterable>)
+#Joins elements of an iterable into a single string. Here, s is the separator string.
+
+#17. s.split(sep = None, maxspit = -1)
+#Splits a string into a list of substrings based on sep. If you don’t pass a value to sep, it splits based on whitespaces.
+
+#18.Index()
+# Python String index() function returns the lowest index where the specified substring is found.
+
+#19.Replace()
+#Python string replace() function is used to create a new string by replacing some parts of another string.
+
+#20.format()
+#It’s used to create a formatted string from the template string and the supplied values.
+
+#21.Startswith()
+# Python string startswith() function returns True if the string starts with the given prefix, otherwise it returns False.
+
+#22.Endswith()
+#Python string endswith() function returns True if the string ends with the given suffix, otherwise it returns False.
+
+
